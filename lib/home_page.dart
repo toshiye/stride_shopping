ed814beb85b3ef32edb99ea771429206cf284897
toshiye <sasakikeni@gmail.com>
2026-03-09 14:00:20 -0300

    comment in unsed test import home_page 20260309

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+/* import 'package:firebase_messaging/firebase_messaging.dart'; */
 import 'app_drawer.dart';
 import 'notification_service.dart';
 
